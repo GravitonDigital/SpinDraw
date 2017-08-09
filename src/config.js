d7c6463a8c9b697058c096d56eafe470bc0f8623
Garlov <oe.nordli@gmail.com>
2017-08-09 14:06:38 +0200

    optimize a bit for mobile, still need some fixes for mobile landscape, but looks fine in portrait

--- src/config.js
+++ src/config.js
@@ -1,9 +1,23 @@
+let height = 600;
+let width = 600;
+if (height > window.innerHeight) {
+  height = innerHeight;
+}
+if (width > window.innerWidth) {
+  width = innerWidth;
+}
+if (width < height) {
+  height = width;
+} else {
+  width = height;
+}
+
 export default {
-  GAMEHEIGHT: 600,
-  GAMEWIDTH: 600,
+  GAMEHEIGHT: width,
+  GAMEWIDTH: height,
   LOCALSTORAGENAME: 'spindraw',
   TITLE: 'Spin Draw',
-  DISKWIDTH_RADIUS: 300,
-  DISKHEIGHT_RADIUS: 300,
+  DISKWIDTH_RADIUS: width/2,
+  DISKHEIGHT_RADIUS: height/2,
   DISKCOLOR: 0xffffff
 }
